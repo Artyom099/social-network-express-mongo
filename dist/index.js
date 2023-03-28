c1105c4b9fbb303496a3680ab3397f2396dcf322
Artyom Golubev <artgolubev@bk.ru>
2023-03-28 21:25:15 +0300

    feat: index.ts - try to make my first endpoints

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// create express app
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello Incubator!!!');
 });
+app.get('/videos', (req, res) => {
+    res.sendStatus(200);
+    res.json({
+        "id": 0,
+        "title": "string",
+        "author": "string",
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": "2023-03-28T17:34:46.859Z",
+        "publicationDate": "2023-03-28",
+        "availableResolution": ["P144"]
+    });
+    res.send('Return all videos');
+});
+app.post('/videos', (req, res) => {
+    res.send('Create new video');
+});
+app.get('/videos/:id', (req, res) => {
+    res.sendStatus(200);
+    const foundVideo = {
+        "id": 0,
+        "title": "string",
+        "author": "string",
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": "2023-03-28T17:34:46.859Z",
+        "publicationDate": "2023-03-28",
+        "availableResolution": ["P144"]
+    }.find(v => v.id === +req.params.id); //найти метод, который применяется к объекту вместо find
+    if (!foundVideo) { // If video for passed id doesn't exist
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundVideo);
+    res.send('Return video by id');
+});
+// start app
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server running on: http://localhost/${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,50 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 
+app.get('/videos', (req: Request, res: Response) => {
+    res.sendStatus(200)
+    res.json({
+        "id": 0,
+        "title": "string",
+        "author": "string",
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": "2023-03-28T17:34:46.859Z",
+        "publicationDate": "2023-03-28",
+        "availableResolution": ["P144"]
+    })
+    res.send('Return all videos')
+})
+app.post('/videos', (req: Request, res: Response) => {
+    res.send('Create new video')
+})
+
+app.get('/videos/:id', (req: Request, res: Response) => {   //добавить try/catch в этот эндпоинт
+    res.sendStatus(200)
+
+    const foundVideo = {
+        "id": 0,
+        "title": "string",
+        "author": "string",
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": "2023-03-28T17:34:46.859Z",
+        "publicationDate": "2023-03-28",
+        "availableResolution": ["P144"]
+    }.find(v => v.id === +req.params.id)    //найти метод, который применяется к объекту вместо find
+
+    if (!foundVideo) {              // If video for passed id doesn't exist
+        res.sendStatus(404)
+        return;
+    }
+    res.json(foundVideo)
+    res.send('Return video by id')
+})
+
+
+
+
 // start app
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server running on: http://localhost/${port}`)
 })
