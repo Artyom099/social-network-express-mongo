747616b6136fdfd6d681551e0840462aa8d61358
Artyom Golubev <artgolubev@bk.ru>
2023-03-29 17:43:42 +0300

    feat: index.ts - in process with POST endpoint

--- dist/index.js
+++ dist/index.js
@@ -7,25 +7,67 @@ const express_1 = __importDefault(require("express"));
 // create express app
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
+const jsonBodyMiddleware = express_1.default.json();
+app.use(jsonBodyMiddleware);
+const db = [{ videos: [{ id: 1, title: 'vid_1' },
+            { id: 2, title: 'vid_2' },
+            { id: 3, title: 'vid_3' },
+            { id: 4, title: 'vid_4' }
+        ]
+    }];
+let errors = [];
+app.get('', (req, res) => {
+    res.send('Hello Incubator!!!');
+});
+app.get('/testing/all-data', (req, res) => {
     res.send('Hello Incubator!!!');
 });
 app.get('/videos', (req, res) => {
-    res.sendStatus(200);
-    res.json({
-        "id": 0,
-        "title": "string",
-        "author": "string",
-        "canBeDownloaded": true,
-        "minAgeRestriction": null,
-        "createdAt": "2023-03-28T17:34:46.859Z",
-        "publicationDate": "2023-03-28",
-        "availableResolution": ["P144"]
-    });
-    res.send('Return all videos');
+    res.status(200).send(db);
 });
 app.post('/videos', (req, res) => {
-    res.send('Create new video');
+    const title = req.body.title;
+    const author = req.body.author;
+    const availableResolutions = req.body.availableResolutions;
+    // validation:
+    if (!title && typeof title !== 'string' && (title.length < 1 || title.length > 40)) {
+        errors.push({
+            message: 'should be a string, max 40 symbols',
+            field: 'title'
+        });
+        return;
+    }
+    if (!author && typeof title !== 'string' && (title.length < 1 || title.length > 40)) {
+        errors.push({
+            message: 'should be a string, max 40 symbols',
+            field: 'author'
+        });
+        return;
+    }
+    if (!availableResolutions && typeof title !== 'arr' && (title.length < 1 || title.length > 40)) {
+        errors.push({
+            message: 'should be not nullable array',
+            field: 'availableResolutions'
+        });
+        return;
+    }
+    const createdVideo = {
+        id: +(new Date()),
+        title: title,
+        author: author,
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions: availableResolutions
+    };
+    res.status(400).send({ errorsMessages: errors });
+    db.videos.push(createdVideo);
+    res.status(201).send(createdVideo);
+    // else
+    // res.status(400)
+    // res.json(errors)
+    // return;
 });
 app.get('/videos/:id', (req, res) => {
     res.sendStatus(200);
@@ -38,7 +80,7 @@ app.get('/videos/:id', (req, res) => {
         "createdAt": "2023-03-28T17:34:46.859Z",
         "publicationDate": "2023-03-28",
         "availableResolution": ["P144"]
-    }.find(v => v.id === +req.params.id); //найти метод, который применяется к объекту вместо find
+    }; // .find(v => v.id === +req.params.id) найти метод, который применяется к объекту вместо find
     if (!foundVideo) { // If video for passed id doesn't exist
         res.sendStatus(404);
         return;
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,85 @@
 import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
+import exp from "constants";
 
 // create express app
 const app = express()
-
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
+const jsonBodyMiddleware = express.json()
+app.use(jsonBodyMiddleware)
+
+const db = [{videos: [  {id: 1, title: 'vid_1'},
+                        {id: 2, title: 'vid_2'},
+                        {id: 3, title: 'vid_3'},
+                        {id: 4, title: 'vid_4'}
+    ]
+}]
+
+let errors = []
+
+
+app.get('', (req: Request, res: Response) => {
+    res.send('Hello Incubator!!!')
+})
+app.get('/testing/all-data', (req: Request, res: Response) => {
     res.send('Hello Incubator!!!')
 })
 
 
 app.get('/videos', (req: Request, res: Response) => {
-    res.sendStatus(200)
-    res.json({
-        "id": 0,
-        "title": "string",
-        "author": "string",
-        "canBeDownloaded": true,
-        "minAgeRestriction": null,
-        "createdAt": "2023-03-28T17:34:46.859Z",
-        "publicationDate": "2023-03-28",
-        "availableResolution": ["P144"]
-    })
-    res.send('Return all videos')
+    res.status(200).send(db)
 })
 app.post('/videos', (req: Request, res: Response) => {
-    res.send('Create new video')
+    const title = req.body.title
+    const author = req.body.author
+    const availableResolutions = req.body.availableResolutions
+    // validation:
+    if (!title && typeof title !== 'string' && (title.length < 1 || title.length > 40)) {
+        errors.push({
+            message: 'should be a string, max 40 symbols',
+            field: 'title'
+        })
+        return;
+    }
+    if (!author && typeof title !== 'string' && (title.length < 1 || title.length > 40)) {
+        errors.push({
+            message: 'should be a string, max 40 symbols',
+            field: 'author'
+        })
+        return;
+    }
+    if (!availableResolutions && typeof title !== 'arr' && (title.length < 1 || title.length > 40)) {
+        errors.push({
+            message: 'should be not nullable array',
+            field: 'availableResolutions'
+        })
+        return;
+    }
+
+    const createdVideo = {
+        id: +(new Date()),
+        title: title,
+        author: author,
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions: availableResolutions
+    }
+    res.status(400).send({errorsMessages: errors})
+
+
+    db.videos.push(createdVideo)
+    res.status(201).send(createdVideo)
+    // else
+    // res.status(400)
+    // res.json(errors)
+    // return;
 })
 
-app.get('/videos/:id', (req: Request, res: Response) => {   //добавить try/catch в этот эндпоинт
+
+app.get('/videos/:id', (req: Request, res: Response) => {   //добавить try/catch в этот эндпоинт??
     res.sendStatus(200)
 
     const foundVideo = {
@@ -41,7 +91,7 @@ app.get('/videos/:id', (req: Request, res: Response) => {   //добавить t
         "createdAt": "2023-03-28T17:34:46.859Z",
         "publicationDate": "2023-03-28",
         "availableResolution": ["P144"]
-    }.find(v => v.id === +req.params.id)    //найти метод, который применяется к объекту вместо find
+    }    // .find(v => v.id === +req.params.id) найти метод, который применяется к объекту вместо find
 
     if (!foundVideo) {              // If video for passed id doesn't exist
         res.sendStatus(404)
