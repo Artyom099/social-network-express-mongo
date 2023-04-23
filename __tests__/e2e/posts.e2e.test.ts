17a1dbadaadcff8814d32e8f90381b02fe1b4afe
Artyom Golubev <artgolubev@bk.ru>
2023-04-23 16:58:16 +0300

    feat: add e2e tests for posts - posts.e2e.test.ts

---
+++ __tests__/e2e/posts.e2e.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import {app} from "../../src/app";
+import {HTTP_STATUS} from "../../src/utils";
+
+
+describe('/posts', () => {
+    beforeAll(async () => {
+        await request(app).delete ('/testing/all-data')
+    })
+
+    it('should return 200 and empty array', async () => {
+        await request(app)
+            .get('/posts')
+            .expect(HTTP_STATUS.OK_200, { pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: [] })
+    })
+
+    it('should return 404 for not existing post', async () => {
+        await request(app)
+            .get('/posts/1')
+            .expect(HTTP_STATUS.NOT_FOUND_404)
+    })
+})
