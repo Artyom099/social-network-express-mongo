d4bb546cf1b5fa21e815734054c2895d6742838a
Artyom <artgolubev@bk.ru>
2023-06-05 12:13:11 +0300

    fix queryRepository

--- src/repositories/query-repository.ts
+++ src/repositories/query-repository.ts
@@ -1,14 +1,15 @@
 import {OutputModel, BlogViewModel, CommentViewModel, PostViewModel, UserAccountDBType} from "../types/types"
-import {commentCollection, postCollection, userCollection} from "../db/db";
+import {userCollection} from "../db/db";
 import {Filter} from "mongodb"
 import {BlogModel} from "../shemas/blogs-schema";
+import {PostModel} from "../shemas/posts-schema";
+import {CommentModel} from "../shemas/feedback-schema";
 
 
 export const queryRepository = {
     async findBlogsAndSort(searchNameTerm: string | null, pageNumber: number, pageSize: number, sortBy: string,
                            sortDirection: 'asc' | 'desc'): Promise<OutputModel<BlogViewModel[]>> {
         const filter = searchNameTerm ? {name: {$regex: searchNameTerm, $options: 'i'}} : {}
-
         const totalCount: number = await BlogModel.countDocuments(filter)
         const sortedBlogs: BlogViewModel[] = await BlogModel.find(filter,{ _id: 0, __v: 0 })
             .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize)
@@ -24,10 +25,9 @@ export const queryRepository = {
     async findPostsThisBlogById(blogId: string, pageNumber: number, pageSize: number, sortBy: string,
                                 sortDirection: 'asc' | 'desc'): Promise<OutputModel<PostViewModel[]>> {   // get
         const filter: {blogId: string} = {blogId: blogId}
-
-        const totalCount: number = await postCollection.countDocuments(filter)
-        const sortedPosts: PostViewModel[] = await postCollection.find(filter, {projection: {_id: 0}})
-            .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize).toArray()
+        const totalCount: number = await PostModel.countDocuments(filter)
+        const sortedPosts: PostViewModel[] = await PostModel.find(filter, {projection: {_id: 0, __v: 0}})
+            .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize).lean()
         return {
             pagesCount: Math.ceil(totalCount / pageSize),    // общее количество страниц
             page: pageNumber,                                   // текущая страница
@@ -39,9 +39,9 @@ export const queryRepository = {
 
     async findPostsAndSort(pageNumber: number, pageSize: number, sortBy: string,
                            sortDirection: 'asc' | 'desc'): Promise<OutputModel<PostViewModel[]>> {
-        const totalCount: number = await postCollection.countDocuments()
-        const sortedPosts: PostViewModel[] = await postCollection.find({}, {projection: {_id: 0}})
-            .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize).toArray()
+        const totalCount: number = await PostModel.countDocuments()
+        const sortedPosts: PostViewModel[] = await PostModel.find({}, {projection: {_id: 0, __v: 0}})
+            .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize).lean()
         return {
             pagesCount: Math.ceil(totalCount / pageSize),    // общее количество страниц
             page: pageNumber,                                   // текущая страница
@@ -53,10 +53,10 @@ export const queryRepository = {
 
     async findUsersAndSort(searchEmailTerm: string | null, searchLoginTerm: string | null, pageNumber: number, pageSize: number, sortBy: string,
                            sortDirection: 'asc' | 'desc'): Promise<OutputModel<UserAccountDBType[]>> {
-        const filter: Filter<UserAccountDBType> = {
-            $or: [{'accountData.login': {$regex: searchLoginTerm ?? '', $options: "i"}},
-                {'accountData.email': {$regex: searchEmailTerm ?? '', $options: "i"}}]
-        }
+        const filter: Filter<UserAccountDBType> = { $or: [
+            { 'accountData.login': {$regex: searchLoginTerm ?? '', $options: "i"} },
+            { 'accountData.email': {$regex: searchEmailTerm ?? '', $options: "i"} }
+        ]}
         const totalCount: number = await userCollection.countDocuments(filter)
         const sortedUsers: UserAccountDBType[] = await userCollection
             .find(filter, {projection: {_id: 0, id: 1, login: '$accountData.login', email: '$accountData.email', createdAt: '$accountData.createdAt'}})
@@ -73,9 +73,9 @@ export const queryRepository = {
     async findCommentsThisPostAndSort(postId: string, pageNumber: number, pageSize: number, sortBy: string,
                                       sortDirection: 'asc' | 'desc'): Promise<OutputModel<CommentViewModel[]>> {
         const filter: {postId: string} = {postId: postId}
-        const totalCount: number = await commentCollection.countDocuments(filter)
-        const sortedComments: CommentViewModel[] = await commentCollection.find(filter, {projection: {_id: 0, postId: 0}})
-            .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize).toArray()
+        const totalCount: number = await CommentModel.countDocuments(filter)
+        const sortedComments: CommentViewModel[] = await CommentModel.find(filter, {projection: {_id: 0, __v: 0, postId: 0}})
+            .sort({[sortBy]: sortDirection}).skip((pageNumber - 1) * pageSize).limit(pageSize).lean()
         return {
             pagesCount: Math.ceil(totalCount / pageSize),    // общее количество страниц
             page: pageNumber,                                   // текущая страница
